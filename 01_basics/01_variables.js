9cfb25a1fe8dc3193d976f277fd92b2b60a77bf6
ullaskumbati <96108518+ullaskumbati@users.noreply.github.com>
2026-03-30 12:24:00 +0000

    basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 12344
+let accountEmail = "ullas@gmail.com"
+var accountPassword = "12345"
+accountCity = "banglore"
+
+// accountId = 2 //not allowed
+
+accountEmail = "kumbati@gmail.com"
+
+console.log(accountId);
