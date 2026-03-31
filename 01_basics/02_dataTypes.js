6d764b28b845be0e25b87dffd34e8ea825ece7bc
ullaskumbati <96108518+ullaskumbati@users.noreply.github.com>
2026-03-31 14:00:54 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+// alert(3+3) //  we are using nodejs not browser
+
+
+let name = "ullas"
+let age = 26
+let loggedIn = false
+
+// number => 2 to pwer 53
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique 
+
+
+//object
+
+
