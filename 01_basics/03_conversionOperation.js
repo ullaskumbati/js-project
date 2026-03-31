788d1a74bbe6466be39cfbadf8c92ef0488bc58a
ullaskumbati <96108518+ullaskumbati@users.noreply.github.com>
2026-03-31 14:18:46 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+let score = null
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(valueInNumber)
+console.log(typeof valueInNumber);
+
+// "33" => 33
+// "33 abc" => NaN
+// true => 1
+// false => 0
+
+let loggedIn = 1;
+
+let booleanValue = Boolean(loggedIn);
+console.log(booleanValue);
+
+// 1 => true; 0 => false
+//  "" => false
+//  "ullas" => true
+
