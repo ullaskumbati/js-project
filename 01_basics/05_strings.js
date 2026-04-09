b2856d3e968d7063a5221aae9a0e493fa07e27b0
ullaskumbati <96108518+ullaskumbati@users.noreply.github.com>
2026-04-09 14:28:09 +0000

    string basics

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -9,3 +9,22 @@ const gameName = new String("ullas kumbati");
 console.log(gameName[0]);
 console.log(gameName.__proto__);
 console.log(gameName.length)
+
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf(' '));
+
+const newString =gameName.substring(0,4);
+console.log(newString);
+
+const anotherString = gameName.slice(-12,4);
+console.log(anotherString);
+
+const newStringOne = "  ullas   ";
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://ullas.com/ullas%28kumbati"
+
+console.log(url.replace('%28','-'));
+
+console.log(url.includes('ullas'));
