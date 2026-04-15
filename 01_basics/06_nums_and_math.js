9020974b3193f6db913a9e0dfbb7aba2a7bf641e
ullaskumbati <96108518+ullaskumbati@users.noreply.github.com>
2026-04-15 14:11:57 +0000

    nums and maths

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,21 @@
+const score = 400;
+console.log(score);
+const balance = new Number(100);
+console.log(balance);
+
+
+console.log(balance.toString().length);
+
+const otherNumber = 123.8966;
+console.log(otherNumber.toPrecision(4));
+
+const hundreds = 100000;
+console.log(hundreds.toLocaleString('en-IN'));
+
+// ++++++++++++++++++++++ MATHS +++++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+
