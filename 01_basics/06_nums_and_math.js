50bfa85f1745e0ceb2a297e311280ce9ca48dd86
ullaskumbati <96108518+ullaskumbati@users.noreply.github.com>
2026-04-20 13:58:14 +0000

    math and nums

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -14,59 +14,14 @@ console.log(hundreds.toLocaleString('en-IN'));
 
 // ++++++++++++++++++++++ MATHS +++++++++++++++++++++++++++
 
-console.log(Math);
-console.log(Math.abs(-4));
-console.log(Math.round(4.6));
-console.log(Math.ceil(4.2));
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.5));
 
-return (
-  <div className="recent-changes-section">
-    <h4 className="recent-changes-title">Recent Changes</h4>
-{groupedChanges.map((group) => (
-  <div key={`cycle-${group.cycleNumber}`} style={{ marginBottom: "20px" }}>
+console.log(Math.random());
+console.log(Math.random()* 10);
 
-    <table className="recent-changes-table">
-      <thead>
-        <tr>
-          <th>Question</th>
-          <th>Old Value</th>
-          <th>New Value</th>
-          <th>Changed On</th>
-        </tr>
-      </thead>
 
-      <tbody>
-        {group.items.map((change, idx) => (
-          <tr key={`${group.cycleNumber}-${idx}`}>
-            <td>
-              {change.isCustomForm ? (
-                <>
-                  {change.questionName}
-                  <br />
-                  <small>
-                    {change.sectionName} → {change.label}
-                  </small>
-                </>
-              ) : (
-                change.questionName
-              )}
-            </td>
-
-            <td className="old-value-cell">
-              {change.oldValue || "-"}
-            </td>
-
-            <td className="new-value-cell">
-              {change.newValue || "-"}
-            </td>
-
-            <td>{formatDate(change.changedAt)}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  </div>
-))}
-  </div>
-);
 
