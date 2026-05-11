f9b798a4701c51d0f8c55b703f65d6d30afb4df8
ullaskumbati <96108518+ullaskumbati@users.noreply.github.com>
2026-05-11 14:14:10 +0000

    arrays

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,1 +1,37 @@
 const myArr = [0,1,2,3,4,5]
+
+
+
+//dates 
+
+let myDate = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023, 0, 23);
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3);
+// let myCreatedDate = new Date("2023-01-14");
+let myCreatedDate = new Date("01-14-2023");
+
+// console.log(myCreatedDate.toLocaleString());
+
+// let myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+
+// console.log(Date.now()/1000);
+
+
+let newDate = new Date();
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+console.log(newDate.getDate());
+
+
+// console.log(`${newDate.getMonth()} and date is `)
+
+newDate.toLocaleString('default',{
+  weekday: "long",
+})
