2925c40ddd2549493e105e9ed859a8ebbf014f76
ullaskumbati <96108518+ullaskumbati@users.noreply.github.com>
2026-05-13 14:28:32 +0000

    arrays

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -3,3 +3,18 @@ const myHeros = ["shaktiman","naags"];
 const myArr2 = new Array(1,2,3,4)
 
 
+const myArr = [0,1,2,3,4,5]
+const myHeros = ["shaktiman","naags"];
+const myArr2 = new Array(1,2,3,4)
+
+
+const myArr = [0,1,2,3,4,5]
+const myHeros = ["shaktiman","naags"];
+const myArr2 = new Array(1,2,3,4)
+
+
+const myArr = [0,1,2,3,4,5]
+const myHeros = ["shaktiman","naags"];
+const myArr2 = new Array(1,2,3,4)
+
+
