d0a7f3672e21e13442e4c2aeca6d0be2f1ebe296
ullaskumbati <96108518+ullaskumbati@users.noreply.github.com>
2026-05-26 14:21:23 +0000

    arrays

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -26,22 +26,18 @@ console.log(user1.email);
 console.log(user2.email);
 
 
-let myName = "ullas kumbati";
-
-let anotherName = myName;
-anotherName = "new name";
-
-console.log(myName);
-console.log(anotherName);
 
 
-let user1 = {
-    email:"user@google.com",
-    upi:"user@ybl"
-}
 
-let user2 =user1;
 
-user2.email = "ullas@google.com";
-console.log(user1.email);
-console.log(user2.email);
+let newDate = new Date();
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+console.log(newDate.getDate());
+
+
+// console.log(`${newDate.getMonth()} and date is `)
+
+newDate.toLocaleString('default',{
+  weekday: "long",
+})
