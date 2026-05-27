5d5c75c579c2e776a92a41c0571bf534d342aa9c
ullaskumbati <96108518+ullaskumbati@users.noreply.github.com>
2026-05-27 13:33:46 +0000

    arrays

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -41,3 +41,53 @@ console.log(newDate.getDate());
 newDate.toLocaleString('default',{
   weekday: "long",
 })
+
+
+
+
+
+
+
+const myArr = [0,1,2,3,4,5]
+const myHeros = ["shaktiman","naags"];
+const myArr2 = new Array(1,2,3,4)
+
+
+
+
+let myName = "ullas kumbati";
+
+let anotherName = myName;
+anotherName = "new name";
+
+console.log(myName);
+console.log(anotherName);
+
+
+let user1 = {
+    email:"user@google.com",
+    upi:"user@ybl"
+}
+
+let user2 =user1;
+
+user2.email = "ullas@google.com";
+console.log(user1.email);
+console.log(user2.email);
+
+
+
+
+
+
+let newDate = new Date();
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+console.log(newDate.getDate());
+
+
+// console.log(`${newDate.getMonth()} and date is `)
+
+newDate.toLocaleString('default',{
+  weekday: "long",
+})
