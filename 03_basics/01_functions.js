46bfcb4fd8f8af81f89475a722f5ceb363d7f94a
ullaskumbati <96108518+ullaskumbati@users.noreply.github.com>
2026-07-06 17:37:56 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -8,8 +8,44 @@ console.log("s");
 
 // sayMyName();
 
+// function add(number1, number2){
+//     console.log(number1 + number2);
+// }
 function add(number1, number2){
-    console.log(number1 + number2);
+   
+    let result = number1 + number2;
+    return result;
 }
 
-add(2,"5");
+const result = add(2,"5");
+// console.log(result);
+
+function loginUserMessage(userName = "sam"){
+    if(userName === undefined){
+        console.log("please ennter username");
+        return;
+    }
+    return `${userName} just logged in`
+}
+
+// console.log(loginUserMessage("ullas"));
+// console.log(loginUserMessage("ullas"));
+
+function calculateCartPrice(var1,var2,...num1){
+    return num1;
+}
+
+// console.log(calculateCartPrice(2,4,5,6,7));
+
+const user ={
+    userName:"ullas",
+    price:77,
+}
+
+function handleObject(anyObject){
+    console.log(`username is ${anyObject.userName} price is ${anyObject.price}`)
+}
+
+handleObject(user);
+
+
