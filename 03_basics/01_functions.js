b12c3c1331143b31d530ce055016dcf5650bd2eb
ullaskumbati <96108518+ullaskumbati@users.noreply.github.com>
2026-07-06 14:30:56 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,15 @@
+function sayMyName(){
+console.log("u");
+console.log("l");
+console.log("l");
+console.log("a");
+console.log("s");
+}
+
+// sayMyName();
+
+function add(number1, number2){
+    console.log(number1 + number2);
+}
+
+add(2,"5");
