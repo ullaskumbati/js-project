a2f70e8cf22606c064080b32d159b664a0476c78
ullaskumbati <96108518+ullaskumbati@users.noreply.github.com>
2026-07-08 18:19:00 +0000

    scopes and arrows

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,8 +1,48 @@
+let a = 300;
+
 if(true){
 let a= 10;
 const b = 20;
 var c = 30;
+// console.log(a);
 }
-console.log(a);
-console.log(b);
-console.log(c);
+
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const userName = "ullas";
+    function two(){
+        const website = "youtube";
+        console.log(userName);
+    }
+    two();
+    console.log(website);
+}
+// one();
+
+if(true){
+    const userName = "ullas";
+    if(userName==="ullas"){
+        const website = " youtube";
+        // console.log(userName + website);
+    }
+    // console.log(website);
+}
+// console.log(userName);
+
+// ++++++++++++++++++++ interesting  ++++++++++++++++
+
+function addone (num){
+    return num +1;
+}
+console.log(addone(5));
+
+const addTwo = function(num){
+    return num + 2
+}
+addTwo(5);
---
+++ 03_basics/03_Arrow.js
@@ -0,0 +1,21 @@
+const user ={
+    userName : "ullas",
+    price:999,
+
+    welcomeMessage : function(){
+        console.log(`${this.userName} welcome to website`)
+        console.log(this);
+    }
+}
+// user.welcomeMessage();
+// user.userName = "maltesh";
+// user.welcomeMessage();
+
+// console.log(this);
+
+// function chai(){
+//     console.log(this);
+// }
+
+// console.log(chai());
+
