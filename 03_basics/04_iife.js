c3f61cfb99af80e6c4966eb27d680c680bed3c13
ullaskumbati <96108518+ullaskumbati@users.noreply.github.com>
2026-07-12 18:18:52 +0000

    iife

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -7,3 +7,36 @@
 ((name)=>{
     console.log(`DB connected two ${name}`);
 })("ullas");
+
+
+
+
+
+
+// Immediately Invoked Function Expression (IIFE)
+
+(function chai(){
+    console.log(`DB connected`);
+})();
+
+((name)=>{
+    console.log(`DB connected two ${name}`);
+})("ullas");
+// Immediately Invoked Function Expression (IIFE)
+
+(function chai(){
+    console.log(`DB connected`);
+})();
+
+((name)=>{
+    console.log(`DB connected two ${name}`);
+})("ullas");
+// Immediately Invoked Function Expression (IIFE)
+
+(function chai(){
+    console.log(`DB connected`);
+})();
+
+((name)=>{
+    console.log(`DB connected two ${name}`);
+})("ullas");
