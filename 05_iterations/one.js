ae687403b1c88f011d245fe2b3a6f29da8b1fffa
ullaskumbati <96108518+ullaskumbati@users.noreply.github.com>
2026-07-15 18:51:40 +0000

    for loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,14 @@
+//for loop
+
+// for (let index = 0; index < 10; index++) {
+//     const element = index;
+//     console.log(element);
+// }
+
+for (let i = 0; i <= 10; i++) {
+    console.log(`outer loop value: ${i}`)
+    for (let j = 0; j <=10; j++) {
+        console.log(`inner loop value${j} and inner loop${i}`);
+        
+    }
+}
