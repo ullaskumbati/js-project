9b8db96754f7134b5113800aa2e815204e44e914
ullaskumbati <96108518+ullaskumbati@users.noreply.github.com>
2026-07-16 14:30:41 +0000

    break and continue

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -5,10 +5,33 @@
 //     console.log(element);
 // }
 
-for (let i = 0; i <= 10; i++) {
-    console.log(`outer loop value: ${i}`)
-    for (let j = 0; j <=10; j++) {
-        console.log(`inner loop value${j} and inner loop${i}`);
-        
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`outer loop value: ${i}`)
+//     for (let j = 1; j <=10; j++) {
+//         // console.log(`inner loop value${j} and inner loop${i}`);
+//         console.log(i + '*' + j + " = " + i*j)
+//     } 
+// }
+let myArray = ["flash","superman","batman"];
+
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    console.log(element);
+}
+
+// break and continue
+
+// for (let index = 1; index < 20; index++) {
+//     if(index==5){
+//         console.log(`detected`);
+//         break;
+//     }
+//     console.log(`value of i is ${index}`);
+// }
+for (let index = 1; index < 20; index++) {
+    if(index==5){
+        console.log(`detected`);
+        continue;
     }
+    console.log(`value of i is ${index}`);
 }
