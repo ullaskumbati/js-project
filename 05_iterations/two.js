653bcea310558f48c6de70330b374ad13c6a295e
ullaskumbati <96108518+ullaskumbati@users.noreply.github.com>
2026-07-22 18:25:19 +0000

    loops

---
+++ 05_iterations/two.js
@@ -0,0 +1,18 @@
+
+let index = 0;
+while (index <=10) {
+    console.log(`value of index ${index}`);
+    index = index + 2;
+}
+
+let myArray = [
+    'flash',
+    "batman",
+    "superman"
+]
+let arr = 0;
+while (arr < myArray.length) {
+    console.log(`value is ${myArray[arr]}`);
+    arr = arr + 1;
+}
+
