7d088aecd0ed7ff2e6b116cf49a5eef0978bc48e
ullaskumbati <96108518+ullaskumbati@users.noreply.github.com>
2026-07-28 14:27:10 +0000

    maps

--- 05_iterations/seven.js
+++ 05_iterations/seven.js
@@ -1,12 +1,16 @@
 const myNumbers = [1,2,3,4,5,6,7,8,9,10];
 
-const newNums = myNumbers.map((num)=> num + 10)
+// const newNums = myNumbers.map((num)=> num + 10)
 // console.log(newNums);
 
 const morNum = [];
 
-const newNum = myNumbers.forEach((item)=>{
-    morNum.push(item + 10);
-})
+// const newNum = myNumbers.forEach((item)=>{
+//     morNum.push(item + 10);
+// })
 // console.log(morNum)
 
+const newNums = myNumbers
+.map((num)=>num * 10)
+.map((num)=>(num + 1)).filter((num)=>(num >=50));
+console.log(newNums);
