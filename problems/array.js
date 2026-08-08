914afa946c45e128e42518a8609c323b96b9170f
ullaskumbati <96108518+ullaskumbati@users.noreply.github.com>
2026-08-08 17:28:58 +0000

    problems

--- problems/array.js
+++ problems/array.js
@@ -1,15 +1,103 @@
-const arr = [1, 2, [3, 2, 3], 5, 6, [8, 2, 5]];
+// 
 
-let result = [];
 
-function flaternArray(array){
-    for(let i=0; i<array.length; i++){
-      if(Array.isArray(array[i])){
-        flaternArray(array[i])
-      }else{
-        result[result.length]=array[i];
-      }
-    }
+// reverse string 
+
+// const str = "kumbati";
+// let result = '';
+
+// for(let i=str.length-1; i>=0; i--){
+//   result += str[i];
+// }
+// console.log(result);
+
+// largest number in array
+
+// const arr = [1,2,33,44,33,556,22,7777,22222,33,44,5567,11];
+
+// let largest = arr[0];
+
+// for(let i=0; i<arr.length; i++){
+//   if(arr[i]> largest){
+//     largest=arr[i];
+//   }
+// }
+// console.log(largest);
+
+//palindrom
+
+// function pal(str){
+//   let left = 0;
+//   let right = str.length-1;
+//   while(right>left){
+//     if(str[left]!== str[right]){
+//       return false;
+//     }
+//     left ++;
+//     right --;
+//   }
+//   return true;
+// }
+// console.log(pal("adam"));
+
+// second largest 
+
+// const arr = [1,2,3,4,5,5,5,3,2,3];
+// let map = new Set(arr);
+// console.log(map);
+// let largest = -Infinity;
+// let secLargest = -Infinity;
+
+// for (let value of map){
+//   if(value>largest){
+//     secLargest=largest;
+//     largest = value;
+
+//   }else if(value>secLargest){
+//     secLargest=value;
+//   }
+// }
+// console.log(secLargest);
+
+// let largest = -Infinity;
+// let secLar = -Infinity;
+// let newValue = new Set(arr);
+// for(let val of newValue){
+//   if(val> largest){
+//     secLar=largest;
+//     largest = val;
+//   }else if(val> secLar){
+//     secLar =val;
+//   }
+// }
+
+// console.log(secLar);
+
+// const arr = [1,2,3,[1,2,3],4,5,[6,5],8];
+
+// let final = [];
+// function rev(arr){
+// for (let i=0; i<arr.length; i++){
+//   if(Array.isArray(arr[i])){
+//     rev(arr[i])
+//   }else{
+//     // final.push(arr[i]);
+//     final[final.length]=arr[i];
+//   }
+// }
+// }
+
+// rev(arr);
+// console.log(final);
+
+// vowel
+
+const val = 'ullas kumbati';
+let vowels = 'aeiou';
+let count = 0;
+for(let i=0; i<val.length-1; i++){
+  if(vowels.includes(val[i])){
+    count ++;
+  }
 }
-flaternArray(arr);
-console.log(result);
+console.log(count);
